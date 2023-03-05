63e78184b2f558ec8e0000260db49f42e2f518f1
Abdullah Odulate <abdulluhodulate@gmail.com>
2023-03-05 09:05:15 +0100

    carousel on click

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const buttons = document.querySelectorAll("[data-carousel-btn]");
 
 buttons.forEach((btn) => {
 	btn.addEventListener("click", () => {
-		const offset = buttons.dataset.carouselButton === "next" ? 1 : -1;
+		const offset = btn.dataset.carouselButton === "next" ? 1 : -1;
 		const slides = btn.closest("[data-carousel]").querySelector("[data-slides");
 
-		const activeSlide = slides.querySelector("[data-active]");
+		const activeSlide = slides.querySelector("[data-active]") ;
+		let newIndex = [...slides.children].indexOf(activeSlide) + offset
+
+		if(newIndex < 0) newIndex = slides.children.length -1
+		if(newIndex >= slides.children.length) newIndex =  0;
+
+		slides.children[newIndex].dataset.active = true
+		delete activeSlide.dataset.active
 	});
 });
